90d1d90fbc73c31a87dcad2631f2039af7a3ea95
Rob Chambers <robchamberspfc@gmail.com>
2021-01-14 16:11:51 +0000

    basic skeleton of app

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # table-to-triples
+
+```npx serve``` 
+
+   ┌───────────────────────────────────────────────────┐
+   │                                                   │
+   │   Serving!                                        │
+   │                                                   │
+   │   - Local:            http://localhost:5000       │
+   │   - On Your Network:  http://192.168.1.105:5000   │
+   │                                                   │
+   │   Copied local address to clipboard!              │
+   │                                                   │
+   └───────────────────────────────────────────────────┘
+
+```node script/index.js {config-name} {input-filename}```
+
+```node script/index.js config data```
---
+++ script/index.js
@@ -0,0 +1,48 @@
+const d3 = require('d3-fetch');
+if (typeof fetch !== 'function') {
+    global.fetch = require('node-fetch-polyfill');
+}
+
+const filePath = 'http://localhost:5000/input/'
+const args = process.argv.slice(2)
+const configFile = args[0]
+const csvName = args[1]
+
+//get the config file specified on script run
+fetch(filePath + configFile + ".json").then((response) => response.json())
+    .then(json => {
+        console.log(json)
+        afterConfigLoaded(json)
+    }).catch(function (error) {
+        console.log(error);
+    });
+
+
+    //write the output csv headers
+function afterConfigLoaded(config) {
+//get the input csv
+d3.csv(filePath + csvName + ".csv").then(function (d) {
+    let output = []
+    console.log(d)
+    //set up loop for all the input data fields
+    // for (i = 1; i < config.input.length; i++) {
+    //     //loop through the input data
+    //     for (j = 0; j < d.length; j++) {
+    //         //get the output headers from the config
+    //         heading1 = config.headers[0].id
+    //         heading2 = config.headers[1].id
+    //         heading3 = config.headers[2].id
+    //         //create the output data, ignoring any fields where there is null
+    //         if (d[j][config.input[i]] != 0) {
+    //             text = {
+    //                 [heading1]: d[j][config.input[0]],
+    //                 [heading2]: d[j][config.input[i]],
+    //                 [heading3]: d.columns[i]
+    //             }
+    //             output.push(text)
+    //         }
+    //     }
+    // }
+})
+
+}
